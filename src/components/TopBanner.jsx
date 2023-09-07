8c7ea28f4535d868d928179378cd5437fb1e4c54
Shaik_Noor <s.noorink@gmail.com>
2023-09-07 15:28:20 +0530

    banner edits

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -2,26 +2,34 @@ import React from 'react';
 import {PiAirplaneTakeoffLight} from 'react-icons/pi';
 import {GoClock} from 'react-icons/go';
 import {FiPhoneCall} from 'react-icons/fi';
+import {BiSolidOffer} from 'react-icons/bi';
 const TopBanner = () => {
     return(
         <>
         <div className='flex justify-between items-center px-4 py-2'>
 
         <div className='flex items-center'>
-        <PiAirplaneTakeoffLight size={30} className='text-black mr-2'/>
+        <PiAirplaneTakeoffLight size={26} className='text-black'/>
         <h1 className='text-xl font-bold text-gray-600'>Traveller</h1>
         </div>
+        
+        
+        <div className='flex m-2 px-4 text-red-600 font-bold items-center'>
+        <BiSolidOffer size={26}/>
+        <h2>50% Discount</h2>
+        </div>
+        
         <div className='flex'>
-        <div className='hidden sm:flex items-center px-6'>
+        <div className='hidden md:flex items-center px-4'>
         <GoClock size={20} className='mr-2'/>
         <p className='text-sm text-gray-700'>8AM - 5PM</p>
         </div>
 
-        <div className='hidden sm:flex items-center px-6'>
+        <div className='hidden md:flex items-center px-4'>
         <FiPhoneCall size={20} className='mr-2'/>
         <p className='text-sm text-gray-700'>+91-9083786321</p>
         </div>
-
+        
         <div>
         <button className='px-4 py-2 border bg-gradient-to-r from-[#0646A7] to-[#3A62AE] text-white'>
         <a href="#book">Book Now</a> 
