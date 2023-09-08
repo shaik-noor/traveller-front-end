58416ca974479e404b18be5636f98ebceb3c5028
Shaik_Noor <s.noorink@gmail.com>
2023-09-08 23:44:23 +0530

    some fixes

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -4,19 +4,19 @@ const Gallery = () => {
             <h2 className="text-center font-bold text-2xl text-gray-800 p-4">Gallery</h2>
             <div className="grid sm:grid-cols-5 gap-2">
                 <div className="sm:col-span-3 row-span-2 col-span-2">
-                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/Singapor.webp" alt="singapore" />
+                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/Singapor.jpg" alt="singapore" />
                 </div>
                 <div>
-                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/maldives.webp" alt="maldives" />
+                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/maldives.jpg" alt="maldives" />
                 </div>
                 <div>
-                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/kateryna-t-RkyYEVHrRbo-unsplash.webp" alt="kateryna" />
+                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/kateryna-t-RkyYEVHrRbo-unsplash.jpg" alt="kateryna" />
                 </div>
                 <div>
-                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/explore-3.webp" alt="explore" />
+                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/explore-3.jpg" alt="explore" />
                 </div>
                 <div>
-                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/hotel-2.webp" alt="hotel-2" />
+                    <img className="w-full h-full object-cover border-4 border-white shadow-lg" src="./assets/hotel-2.jpg" alt="hotel-2" />
                 </div>
             </div>
         </div>
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -3,15 +3,15 @@ const MainSection = () =>{
         <div id="about" className="max-w-[1200px] m-auto w-full md:flex mt-[-75px]">
             <div className="relative p-4">
                 <h3 className="absolute z-10 text-2xl top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-bold text-white">Destinations</h3>
-                <img className ="w-full h-full object-cover relative border-4 border-white shadow-lg"src="./assets/Dubai.webp" alt="dubai" />
+                <img className ="w-full h-full object-cover relative border-4 border-white shadow-lg"src="./assets/Dubai.jpg" alt="dubai" />
             </div>
             <div className="relative p-4">
                 <h3 className="absolute z-10 text-2xl top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-bold text-white">Hotels</h3>
-                <img className ="w-full h-full object-cover relative border-4 border-white shadow-lg"src="./assets/hotel-1.webp" alt="hotel" />
+                <img className ="w-full h-full object-cover relative border-4 border-white shadow-lg"src="./assets/hotel-1.jpg" alt="hotel" />
             </div>
             <div className="relative p-4">
                 <h3 className="absolute z-10 text-2xl top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] font-bold text-white">Explore</h3>
-                <img className ="w-full h-full object-cover relative border-4 border-white shadow-lg"src="./assets/explore-2.webp" alt="under-water" />
+                <img className ="w-full h-full object-cover relative border-4 border-white shadow-lg"src="./assets/explore-2.jpg" alt="under-water" />
             </div>
         </div>
     )
